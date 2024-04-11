bd3ec877d3e563ef23277f7bd0ed7fe0874ed8b4
sivakowshik05 <sivakowshikcode@gmail.com>
2024-04-11 12:32:01 +0530

    blobs

---
+++ python basics-2.py
@@ -0,0 +1,6 @@
+print(bin(5))
+print(int('0b101', 2))
+
+
+weather = "\t It\'s \"kind of\"sunny \n hope you have a good day!"
+print(weather)
---
+++ python basics-3.py
@@ -0,0 +1,11 @@
+name = "Buri Buri"
+age = 17
+
+print("hi " + name, "you are " + str(age) + " years old")
+
+print("hi {new_name}. you are {age} years old".format(new_name = "Blob", age = 16))
+# or 
+# print(f"hi {name}. you are {age} years old")
+
+# or
+# print("hi {}. You are {} years old".format("Buri Buri", "17"))
