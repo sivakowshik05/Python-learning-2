1f9052636a64895c0fbc81acd8c403341eeb30b7
sivakowshik05 <sivakowshikcode@gmail.com>
2024-04-12 23:55:54 +0530

    buriburi-4

---
+++ python basics-7.py
@@ -0,0 +1,6 @@
+amazon_cart = ["football", "boots", "stockings", "skins"]
+print(amazon_cart[0])
+# this is known as a list
+# for an eg l1 = [1, 2, 3, 4]
+# or l2 = [a, b, c, d]
+# or l3 = [a, 2, true, 3] even booleans can be added in lists
