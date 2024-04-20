f136a53d33c38db2135665060c9b4c6cf5cc2f01
sivakowshik05 <sivakowshikcode@gmail.com>
2024-04-20 19:50:41 +0530

    buriburi-10

---
+++ python basics-12.py
@@ -0,0 +1,37 @@
+dictionary1 = {
+    "a" : 1,
+    "b" : 2,
+    "c" : 3
+} 
+
+print(dictionary1["a"])
+print(dictionary1["b"])
+print(dictionary1["c"])
+print(dictionary1)
+
+dictionary2 = {
+    "a" : [1, 2, 3],
+    "b" : 'hello',
+    "c" : True
+}
+
+
+print(dictionary2["a"])
+print(dictionary2["b"])
+print(dictionary2["c"])
+print(dictionary2)
+print(dictionary2["a"][1])
+
+buriburi_list =  {
+    "a" : [1, 2, 3],
+    "b" : 'hello',
+    "c" : True
+},
+{
+    "a" : [4, 5, 6],
+    "b" : 'hello',
+    "c" : True
+}
+
+print(buriburi_list[0]["a"][2])
+
