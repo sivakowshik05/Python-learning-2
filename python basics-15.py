c4b047c5eeb7f9148936439d249ce9a2a0318476
sivakowshik05 <sivakowshikcode@gmail.com>
2024-04-24 18:53:25 +0530

    buriburi-14

---
+++ python basics-15.py
@@ -0,0 +1,13 @@
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple(1))
+print(5 in my_tuple) 
+new_tuple1 = my_tuple[1:2]
+new_tuple2 = my_tuple[1:4]
+x = my_tuple[1]
+y = my_tuple[2]
+a, b, c, *other  = (1, 2, 3, 4, 5)
+print(my_tuple.count(5))
+my_tuple3 = (1, 2, 3, 4, 5, 5, 5) 
+print(my_tuple3.count(5))
+print(my_tuple.index(5))
+print(len(my_tuple))
