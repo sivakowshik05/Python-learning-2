2712e02c801ff82ec0b10b94aa57f361b32147b5
sivakowshik05 <sivakowshikcode@gmail.com>
2024-05-31 15:18:04 +0530

    buriburi-18

---
+++ python basics-20.py
@@ -0,0 +1,12 @@
+# ternary operator
+
+# condition_if_true if condition else condition_if_false
+# for True condition
+is_friend1 = True
+can_message1 = "message allowed" if is_friend1 else "not allowed to message"
+print(can_message1)
+
+# for false condition
+is_friend2 = False
+can_message2 = "message allowed" if is_friend2 else "not allowed to message"
+print(can_message2)
