e206ebfb489f1099d79a650ae08fd11a21bf3dba
sivakowshik05 <sivakowshikcode@gmail.com>
2024-05-31 16:53:19 +0530

    buriburi-20

---
+++ python basics-22.py
@@ -0,0 +1,37 @@
+is_magician = False
+is_expert = True
+
+if is_expert and is_magician:
+    print("you are a master magician")
+
+elif is_magician and not is_expert:
+    print("atleast you're getting there")
+
+elif not is_magician:
+    print("you need magic powers")  
+
+# if is_magician is false none of these would work
+    
+is_magician2 = True
+is_expert2 = True
+
+if is_expert2 and is_magician2:
+    print("you are a master magician")
+
+elif is_magician2 and not is_expert2:
+    print("atleast you're getting there")
+
+elif not is_magician2:
+    print("you need magic powers")
+
+is_magician3 = True
+is_expert3 = False
+
+if is_expert3 and is_magician3:
+    print("you are a master magician")
+
+elif is_magician3 and not is_expert3:
+    print("atleast you're getting there")
+
+elif not is_magician3:
+    print("you need magic powers")    
