a80274e8682e2dd1588754bdc20027cd307a6c6e
sivakowshik05 <sivakowshikcode@gmail.com>
2024-06-01 13:22:25 +0530

    for loops

--- python basics-24.py
+++ python basics-24.py
@@ -7,7 +7,7 @@ for item in {1, 2, 3, 4, 5}:
 for item in (1, 2, 3, 4, 5):
     print(item) # this can also be applied in tuples
     print(item)
-    print(item) # now it each thing will be printed twice
+    print(item) # now each thing will be printed twice
 print(item) # now, here only five will be printed four times because the value of item is the last thing in the list that is here the value of item is five so five will be printed four times
 print('hi!') # the hi! will be printed once atlast because it is not in the list
 for item in (1, 2, 3, 4, 5):
