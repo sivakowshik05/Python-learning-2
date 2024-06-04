9c5020fd603981379773a4e77a4cf1e7974a804e
sivakowshik05 <sivakowshikcode@gmail.com>
2024-06-04 11:55:03 +0530

    break,continue and pass

---
+++ python basics-30.py
@@ -0,0 +1,42 @@
+my_list = [1, 2, 3]
+for item in my_list:
+    print(item)
+    break
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    break
+
+my_list = [1, 2, 3]
+for item in my_list:
+    print(item)
+    continue
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    continue
+
+my_list = [1, 2, 3]
+for item in my_list:
+    print(item)
+    pass
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+
+my_list = [1, 2, 3]
+for item in my_list:
+    pass
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
